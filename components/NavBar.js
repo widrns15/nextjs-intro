d4a464aaafa999462bfd86726e8aa42ad857e5f0
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-11-16 00:52:23 +0900

    🥽 test: Styles JSX로 CSS 설정해보기

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,28 +1,30 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link
-        href="/"
-        className={`${styles.link} ${
-          router.pathname === "/" ? styles.active : ""
-        }`}
-      >
+      <Link href="/" className={router.pathname === "/" ? "active" : ""}>
         Home
       </Link>
       <Link
         href="/about"
-        className={[
-          styles.link,
-          router.pathname === "/about" ? styles.active : "",
-        ].join(" ")}
+        className={router.pathname === "/about" ? "active" : ""}
       >
         About
       </Link>
+      <style jsx global>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 }
