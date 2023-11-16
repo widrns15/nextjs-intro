b39a650738b54e6bd24337393433d353739bab36
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-11-17 02:53:43 +0900

    💄 design: 페이지 CSS 디자인 적용

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,15 +5,47 @@ export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link href="/" className={router.pathname === "/" ? "active" : ""}>
-        Home
-      </Link>
-      <Link
-        href="/about"
-        className={router.pathname === "/about" ? "active" : ""}
-      >
-        About
-      </Link>
+      <img src="xxx" />
+      <div>
+        <Link href="/" className={router.pathname === "/" ? "active" : ""}>
+          Home
+        </Link>
+        <Link
+          href="/about"
+          className={router.pathname === "/about" ? "active" : ""}
+        >
+          About
+        </Link>
+      </div>
+      <style jsx global>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+          text-decoration: none;
+          color: black;
+        }
+        .active {
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+      `}</style>
     </nav>
   );
 }
