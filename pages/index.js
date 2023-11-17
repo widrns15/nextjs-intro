9b7ab157ad12cd5468718d7ba5442ab1d46859f4
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-11-18 06:31:18 +0900

    🔥 feat: 영화 데이터 Fetching 로직 추가

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,32 @@
 import Seo from "@/components/Seo";
+import { useEffect, useState } from "react";
+
+const API_KEY = "xxx";
 
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+      console.log(results);
+    })();
+  }, []);
+
   return (
     <div>
       <Seo title="Home" />
-      <h1 className="active">Home</h1>
+      {!movies.length && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
