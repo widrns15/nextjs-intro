3acf25e0a5c93b6f1d65d72b19b2522cfb8f7fe0
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-11-19 01:02:39 +0900

    ⚙️ chore: next.config에서 API 마스킹 및 리다이렉션 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
 
-module.exports = nextConfig
+  async redirects() {
+    return [
+      {
+        source: "/contact/:path*", // 들어오는 request 경로 패턴
+        destination: "/form/:path*", // 라우팅하려는 경로 (redirect할 경로)
+        permanent: false, // 브라우저가 계속 기억해야 하는 여부
+      },
+    ];
+  },
+
+  async rewrites() {
+    // redirect랑 비슷하지만, url이 변경되지 않음
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,12 @@
 import Seo from "@/components/Seo";
 import { useEffect, useState } from "react";
 
-const API_KEY = "xxx";
-
 export default function Home() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
+      const { results } = await (await fetch(`/api/movies`)).json();
       setMovies(results);
       console.log(results);
     })();
