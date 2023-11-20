8f531c6f0f087ab84515ff1235a3f99f83f93410
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-11-21 03:10:05 +0900

    🛠️ fix: 404 페이지 문법 오류 수정

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,5 @@
 export default function NotFound() {
   return (
-    <div style={{ padding: "20px", textAlign: "center" }}>It's Not Found</div>
+    <div style={{ padding: "20px", textAlign: "center" }}>It is Not Found</div>
   );
 }
